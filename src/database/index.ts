3a479b690b30dc50d25deaa5435719d365841d48
Vinit Raut <vinit.coder@gmail.com>
2024-01-14 06:43:08 -0800

    Add: Initial commit with auth and user APIs

---
+++ src/database/index.ts
@@ -0,0 +1,14 @@
+import { connect, set } from 'mongoose';
+import { NODE_ENV, DB_URI } from '@config';
+
+export const dbConnection = async () => {
+  const dbConfig = {
+    url: DB_URI
+  };
+
+  if (NODE_ENV !== 'production') {
+    set('debug', true);
+  }
+
+  await connect(dbConfig.url);
+}
